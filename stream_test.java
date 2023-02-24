e3e4ea3e648281901587328ca96fb124dfeb2157
RLYC233 <56778123+raymond-cy@users.noreply.github.com>
2023-02-23 16:45:10 -0800

    It works, but doesn't work as intent

--- stream_test.java
+++ stream_test.java
@@ -1,14 +1,34 @@
-import java.util.Scanner;
-public class stream_test{
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        String str = new String();
-        while(true){
-            str = scan.nextLine();
-            System.out.println(str);
-            if(str.equals("v")){
-                break;
-            }
-        }
+package test;
+
+import java.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.event.*;
+
+@SuppressWarnings("serial")
+public class stream_test extends JFrame{
+    public stream_test(){
+        input command = new input();
+        this.add(command);
+        this.addKeyListener(command);
+        this.setSize(500, 500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
+    public static void main(String[] args) {
+        new stream_test();
+    }
+}
+
+@SuppressWarnings("Serial")
+class input extends JPanel implements KeyListener{
+    @Override
+    public void keyTyped(KeyEvent e){}
+    @Override
+    public void KeyPressed(KeyEvent e){
+        System.out.println(e.getKeyChar() + "Click");
+    }
+    @Override
+    public void keyReleased(KeyEvent e){}
 }
